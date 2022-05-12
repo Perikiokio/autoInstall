3aa18eb879724f9acb77878d8d4ee09077e770ff
Perikiokio <90554098+Perikiokio@users.noreply.github.com>
2022-05-12 17:13:11 +0200

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,11 @@
+apt-get update
+apt-get install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+mkdir /docker
+mkdir /docker/wordpress
+cd /docker/wordpress
+wget https://github.com/Perikiokio/autoInstall/blob/main/docker-compose.yml
+docker compose up -d
